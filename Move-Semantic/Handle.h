551813816c82f6d9d24a7b914cf8a0e6b644eafc
Alexey Malov <alexey.malov@cpslabs.net>
2017-08-23 00:49:55 +0300

    Доработан класс Handle

--- Move-Semantic/Handle.h
+++ Move-Semantic/Handle.h
@@ -2,19 +2,22 @@
 #include <windows.h>
 
 namespace detail {
-template <HANDLE INVALID>
-class Handle {
+template <HANDLE INVALID> class Handle {
 public:
   Handle() = default;
   explicit Handle(HANDLE handle = INVALID) : m_handle(handle) {}
-  Handle(Handle&& x) : m_handle(x.m_handle) { x.m_handle = INVALID; }
+  Handle(Handle &&x) noexcept : m_handle(x.m_handle) { x.m_handle = INVALID; }
   Handle(const Handle &&) = delete; // no copy construction
 
-  Handle &operator=(Handle&& x) { // move assignment
-    std::swap(m_handle, x.m_handle);
+  Handle &operator=(Handle &&x) { // move assignment
+    if (this != &x) {
+      Close();
+      m_handle = x.m_handle;
+      x.m_handle = INVALID;
+    }
     return *this;
   }
-  Handle &operator=(const Handle&) = delete; // no copy assignment
+  Handle &operator=(const Handle &) = delete; // no copy assignment
 
   ~Handle() {
     if (m_handle != INVALID) {
@@ -22,15 +25,15 @@ public:
     }
   }
 
-  explicit operator bool() const { return m_handle != INVALID; }
-  operator HANDLE() const { return m_handle; }
+  explicit operator bool() const noexcept { return m_handle != INVALID; }
+  operator HANDLE() const noexcept { return m_handle; }
 
   void Close() {
-    if (!CloseHandle(m_handle)) {
+    if (m_handle != INVALID) {
+      if (!CloseHandle(m_handle))
+        throw std::runtime_error("Failed to close handle");
       m_handle = INVALID;
-      throw std::runtime_error("Failed to close handle");
     }
-    m_handle = INVALID;
   }
 
 private:
